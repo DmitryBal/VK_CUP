90fd474a469cd75acf1765dc711c913660a5bcbe
DmitryBal <76563919+DmitryBal@users.noreply.github.com>
2023-03-17 05:54:08 +0300

    Add files via upload

---
+++ JSON_to_SQL.ps1
@@ -0,0 +1,27 @@
+﻿function t{
+    param(
+        [string] $str,
+        [int] $count
+    )
+    PROCESS
+    {
+        $pos = $str.IndexOf('comments')
+        $right = $str.Substring($pos+1)
+        $left = $str.Substring(0,$pos)
+        $left = $left -replace '{"text":"',"insert into Post(id, title) VALUES ($count,'"
+        $left = $left -replace '","',"');"
+        $trash = $right.IndexOf('4}]}')
+        $count+=1
+        echo $left
+        if($count -le 500){
+            $right = $right.Substring($trash+5)
+            t $right $count
+        }
+    }
+}
+
+$text  = Get-Content "C:\Users\dimab\Downloads\text.txt"
+$count = 1.0
+$text = $text -replace "'", "''"
+$test = t $text $count
+Out-File -FilePath C:\Users\dimab\Downloads\text_1.txt -InputObject $test -Encoding ASCII -Width 50
---
+++ comments.ps1
@@ -0,0 +1,38 @@
+﻿function comment{
+    param(
+        [string] $str,
+        [int] $count,
+        [int] $j
+    )
+    PROCESS
+    {
+        
+        $end = $str.IndexOf(']}')
+        $start = $str.IndexOf('comments')
+        $split = $str.Substring($start+11)
+        $split = $split -replace '{"text":"', "insert into Comments(id,post_id, content, rating) VALUES'"
+        $split = $split -replace '","score":',"', "
+        $split = $split -replace '},' , ');'
+        $split = $split -replace '}]' , ');'
+        for($i = 0; $i -le 4;$i++){
+            $half = $split.IndexOf(');')
+            $line = $split.Substring(0,$half+2)
+            $line = $line -replace 'VALUES', "VALUES ($j, $count, "
+            $line 
+            $j+=1
+            $split = $split.Substring($half+2)
+        }
+        $count+=1
+        if($count -le 500){
+            $right = $str.Substring($end+3)
+           comment $right $count $j
+        }
+    }
+}
+
+$text  = Get-Content "C:\Users\dimab\Downloads\text.txt"
+$count = 1.0
+$j = 1.0
+$text = $text -replace "'", "''"
+$test_2 = comment $text $count $j
+Out-File -FilePath C:\Users\dimab\Downloads\text_2.txt -InputObject $test_2 -Encoding ASCII 
